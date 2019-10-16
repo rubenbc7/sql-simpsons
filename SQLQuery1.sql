89c67456affbb7e8f15f82a1c00a4f45dfe4291c
Rubén Borbolla <53828155+rubenbc7@users.noreply.github.com>
2019-10-15 20:40:00 -0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+use db_simpsons;
+
+insert into tbl_character(firstName, secondName, lastName, age, descp)
+values('Lisa', '', 'Simpson', 8, 'Toca el saxofón y tiene el pelo de estrealla');
+
+select * from tbl_character
+
+
